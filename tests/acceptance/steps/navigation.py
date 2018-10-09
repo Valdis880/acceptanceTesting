50f985fd96b22310645c6bd6f67f5b3c183794da
Valdis880 <va.aksenov@bk.ru>
2018-10-09 13:16:40 +0700

    implement content tests

---
+++ tests/acceptance/steps/navigation.py
@@ -0,0 +1,35 @@
+from behave import *
+from selenium import webdriver
+
+from acceptanceTesting.tests.acceptance.page_model.blog_page import BlogPage
+from acceptanceTesting.tests.acceptance.page_model.home_page import HomePage
+
+use_step_matcher('re')
+
+
+@given('I am on the homepage')
+def step_impl(context):
+    context.driver = webdriver.Chrome('/Users/Vladislav_Aks/Desktop/acceptanceTesting/acceptanceTesting/tests/'
+                                       'acceptance/steps/chromedriver')
+    page = HomePage(context.driver)
+    context.driver.get(page.url)
+
+
+@given('I am on the blog page')
+def step_impl(context):
+    context.driver = webdriver.Chrome('/Users/Vladislav_Aks/Desktop/acceptanceTesting/acceptanceTesting/tests/'
+                                       'acceptance/steps/chromedriver')
+    page = BlogPage(context.driver)
+    context.driver.get(page.url)
+
+
+@then('I am on the blog page')
+def step_impl(context):
+    expected_url = BlogPage(context.driver).url
+    assert context.driver.current_url == expected_url
+
+
+@then('I am on the homepage')
+def step_impl(context):
+    expected_url = HomePage(context.driver).url
+    assert context.driver.current_url == expected_url
