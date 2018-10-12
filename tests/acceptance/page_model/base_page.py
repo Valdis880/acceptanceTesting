1a8de0adb45ebb46e4791aba7458c3fc412cfdb1
Valdis880 <va.aksenov@bk.ru>
2018-10-12 10:07:26 +0700

    final test suite

--- tests/acceptance/page_model/base_page.py
+++ tests/acceptance/page_model/base_page.py
@@ -5,7 +5,6 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
-
     @property
     def url(self):
         return 'http://127.0.0.1:5000'
@@ -13,3 +12,7 @@ class BasePage:
     @property
     def title(self):
         return self.driver.find_element(*BasePageLocators.TITLE)
+
+    @property
+    def navigation(self):
+        return self.driver.find_elements(*BasePageLocators.NAV_LINKS)
--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 from acceptanceTesting.tests.acceptance.page_model.blog_page import BlogPage
 from acceptanceTesting.tests.acceptance.page_model.home_page import HomePage
+from acceptanceTesting.tests.acceptance.page_model.new_post_page import NewPostPage
 
 use_step_matcher('re')
 
@@ -10,7 +11,7 @@ use_step_matcher('re')
 @given('I am on the homepage')
 def step_impl(context):
     context.driver = webdriver.Chrome('/Users/Vladislav_Aks/Desktop/acceptanceTesting/acceptanceTesting/tests/'
-                                       'acceptance/steps/chromedriver')
+                                      'acceptance/steps/chromedriver')
     page = HomePage(context.driver)
     context.driver.get(page.url)
 
@@ -18,8 +19,16 @@ def step_impl(context):
 @given('I am on the blog page')
 def step_impl(context):
     context.driver = webdriver.Chrome('/Users/Vladislav_Aks/Desktop/acceptanceTesting/acceptanceTesting/tests/'
-                                       'acceptance/steps/chromedriver')
+                                      'acceptance/steps/chromedriver')
     page = BlogPage(context.driver)
+    context.driver.get(page.url)
+
+
+@given('I am on the new post page')
+def step_impl(context):
+    context.driver = webdriver.Chrome('/Users/Vladislav_Aks/Desktop/acceptanceTesting/acceptanceTesting/tests/'
+                                      'acceptance/steps/chromedriver')
+    page = NewPostPage(context.driver)
     context.driver.get(page.url)
 
 
